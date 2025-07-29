c4a29682c4b2b72027e6bc72c8b3564b6ce2cf07
Valencia Masika <masikavalencia62@gmail.com>
2025-07-29 19:29:47 +0300

    Fuckit

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
+# Custom display settings for the Book model in the admin panel
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('publication_year',)
+    search_fields = ('title', 'author')
 
-# Register your models here.
+admin.site.register(Book, BookAdmin)
