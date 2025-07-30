9c71aae27434a45c9112c2c3dad487b7c6667ba6
Valencia Masika <masikavalencia62@gmail.com>
2025-07-30 10:08:32 +0300

    Fix query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,17 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author = Author.objects.get(name="George Orwell")
+author_name = "George Orwell"
+author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
-print("Books by George Orwell:", books_by_author)
+print("Books by", author_name, ":", books_by_author)
 
 # List all books in a library
-library = Library.objects.get(name="Central Library")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
-print("Books in Central Library:", books_in_library)
+print("Books in", library_name, ":", books_in_library)
 
 # Retrieve the librarian for a library
 librarian = Librarian.objects.get(library=library)
-print("Librarian of Central Library:", librarian)
+print("Librarian of", library_name, ":", librarian)
