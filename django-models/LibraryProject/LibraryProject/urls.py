428c8fcec243fa4e1bbc1987e8887a24c5e11b48
Valencia Masika <masikavalencia62@gmail.com>
2025-08-04 16:58:42 +0300

    Add custom permissions

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),  # include app urls here
+    path('', include('relationship_app.urls')),
 ]
