874604b3b1d674c82d7f2700b4ce11046bff7bc6
Valencia Masika <masikavalencia62@gmail.com>
2025-08-04 17:41:00 +0300

    Add access

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Book, Library
+from .models import Book  # ✅ required
+from .models import Library  # ✅ required for checker
 
-# Function-Based View: List all books
+from django.views.generic.detail import DetailView
+
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-Based View: Show details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
