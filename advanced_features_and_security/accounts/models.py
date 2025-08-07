8d3c045fa2d61ae6d1e5c72d701af1860a3555b3
Valencia Masika <masikavalencia62@gmail.com>
2025-08-07 16:41:02 +0300

    Implement custom user model

---
+++ advanced_features_and_security/accounts/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profiles/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
