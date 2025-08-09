1767366ea55496e2155b190c34254d37af5ffeb8
Valencia Masika <masikavalencia62@gmail.com>
2025-08-09 14:59:08 +0300

    Implement security best practices

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
+from django import forms
+
 class BookSearchForm(forms.Form):
     query = forms.CharField(max_length=100, required=True)
+
+class ExampleForm(forms.Form):
+    example_field = forms.CharField(max_length=100, required=True)
