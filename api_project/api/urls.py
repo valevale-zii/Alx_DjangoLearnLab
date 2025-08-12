af0fdea385e74473d7d69c34da78d0047c935ab1
Valencia Masika <masikavalencia62@gmail.com>
2025-08-12 15:46:51 +0300

    Setting up DRF

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import BookList
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+    path('books/', BookList.as_view(), name='book-list'),  # ✅ using path()
 ]
