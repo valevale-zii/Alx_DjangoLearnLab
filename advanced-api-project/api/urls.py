f42cdbe9bb6dbb0da9d5ca14349121ed7ab5dd68
Valencia Masika <masikavalencia62@gmail.com>
2025-08-23 19:44:10 +0300

    Setup Django Project

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import BookListCreateAPI, BookDetailAPI
+from .views import (
+    BookListView,
+    BookDetailView,
+    BookCreateView,
+    BookUpdateView,
+    BookDeleteView,
+)
 
 urlpatterns = [
-    path('books/', BookListCreateAPI.as_view(), name='api-book-list'),
-    path('books/<int:pk>/', BookDetailAPI.as_view(), name='api-book-detail'),
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
 ]
