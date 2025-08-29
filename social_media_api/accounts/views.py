c953f5111ed4f8040075424bb2a079ac9cc418e6
Valencia Masika <masikavalencia62@gmail.com>
2025-08-29 15:00:31 +0300

    Social Media App

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,26 +1,24 @@
 from rest_framework import generics, permissions
-from django.contrib.auth import authenticate
-from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
-from rest_framework.views import APIView
-from .serializers import RegisterSerializer, UserSerializer
+from rest_framework.response import Response
 from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, UserSerializer
 
 User = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
 
-class LoginView(APIView):
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username, password=password)
-        if user:
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response({"token": token.key})
-        return Response({"error": "Invalid Credentials"}, status=400)
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data["token"])
+        return Response({"token": token.key, "user_id": token.user_id, "username": token.user.username})
+
 
 class ProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
